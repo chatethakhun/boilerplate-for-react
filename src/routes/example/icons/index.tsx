eb051b801766f9085bf122eed73b4105c3ebcbfc
Chatethakhun Khongthon <chatethakhunkhongthon@Chatethakhuns-MacBook-Air.local>
2025-07-23 16:01:03 +0700

    add function copy code icon

--- src/routes/example/icons/index.tsx
+++ src/routes/example/icons/index.tsx
@@ -1,11 +1,15 @@
 import { createFileRoute } from '@tanstack/react-router'
 import * as Icons from 'omoo-icons'
+import { useCallback } from 'react'
 
 export const Route = createFileRoute('/example/icons/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  const onCopy = useCallback((iconName: string) => {
+    navigator.clipboard.writeText(`<${iconName} />`)
+  }, [])
   return (
     <div className="flex flex-col gap-2 flex-wrap px-2">
       {Object.entries(Icons).map(([name, Icon]) => (
@@ -13,7 +17,10 @@ function RouteComponent() {
           <div className="flex items-center justify-center w-4 h-4 ">
             <Icon className="text-sm text-gray-700" />
           </div>
-          <div className="text-sm">{name}</div>
+          <code
+            className="cursor-pointer bg-blue-950 px-2 text-white rounded-md"
+            onClick={() => onCopy(name)}
+          >{`<${name} />`}</code>
         </div>
       ))}
     </div>
