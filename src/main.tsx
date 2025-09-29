cacad26bb11c400033ad2fa8c1df71e4523c1b51
“The <chatethakhun.jack@gmail.com>
2025-09-29 10:26:32 +0700

    Add react-scan and enable in development
    - Install react-scan and initialize in dev mode - Bump axios to 1.12.2
    and vite to 6.3.6

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
-
+import { scan } from 'react-scan'
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
 
 import './styles.css'
 import reportWebVitals from './reportWebVitals.ts'
+
+if (process.env.NODE_ENV === 'development') {
+  scan({ enabled: true })
+}
 
 // Create a new router instance
 const router = createRouter({
